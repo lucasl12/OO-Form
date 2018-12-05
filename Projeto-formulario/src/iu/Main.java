a2ea522d49e5e2c6225ae44cfec3d9c606f49606
Lucas Lopes <lucaslopes05561@gmail.com>
2018-12-05 13:38:29 -0200

    Versao2.0

--- Projeto-formulario/src/iu/Main.java
+++ Projeto-formulario/src/iu/Main.java
@@ -1,45 +1,17 @@
 package iu;
 
-import java.util.ArrayList;
-
-import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-import javax.swing.plaf.synth.SynthSeparatorUI;
 
-import formulario.Formulario;
-
-import opcoes.Tela;
 import perguntas.Pergunta;
 import perguntasAbertas.TextoCurto;
 import perguntasAbertas.TextoLongo;
-import perguntasFechadas.Alternativa;
-import perguntasFechadas.Lista;
-import perguntasFechadas.Opcional;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		
-		
-		  
-        
-	     
-		 ArrayList<Pergunta>p = new ArrayList<Pergunta>(); // Um Lista de perguntas, pra dps passar por parametro.
-		 Pergunta perg; // referencia gênerica para pergunta
-		 
-		  String items[]= {};
-	
-		  
-		  int quantidadedeperguntasabertas=0;
-		  int quantidadedeperguntasfechadasAlt=0;
-		  int quantidadedeperguntasfechadasList=0;
-		  int quantidadedeperguntasfechadasOp=0;
-		  int quantidadeitens=0;
-		 
-		  
-		 //criação do campo de obter informações do formulario
+		//criação do campo de obter informações do formulario
 		 
 		JTextField Nomef = new JTextField(); // Criando o campo nome
 		JTextField Descf = new JTextField();// 
@@ -65,121 +37,109 @@ public class Main {
 		String DataFinal = Datafinal.getText().toString();
 		int quantidadeperg = Integer.parseInt(QuantidadePerg.getText().toString());
 
-		//array que vai armazenart todos os enuciados
-		  String enunciados[] = new String[quantidadeperg];
-		    for(int i=0;i<quantidadeperg;i++) {
-			String tipo =JOptionPane.showInputDialog("Qual tipo da " + (1+i)+"° pergunta deseja criar?");
-				if(tipo.equalsIgnoreCase("texto curto") || tipo.equalsIgnoreCase("textocurto")) {
-					String enuciado = JOptionPane.showInputDialog("Qual o enuciado?");
-					perg = new TextoCurto();// Usando o polimorfismo
-					perg.setEnuciadoPergunta(enuciado);
-		                        enunciados[i]= perg.getEnuciadoPergunta();
-		                        quantidadedeperguntasabertas= quantidadedeperguntasabertas+1;
-		                        
-		                }
-		                if(tipo.equalsIgnoreCase("texto longo") || tipo.equalsIgnoreCase("textolongo")){
-		                    String enuciado = JOptionPane.showInputDialog("Qual o enuciado?" + (1+i));
-		                    perg = new TextoLongo();
-		                    perg.setEnuciadoPergunta(enuciado);
-		                    enunciados[i] = perg.getEnuciadoPergunta();
-		                    quantidadedeperguntasabertas= quantidadedeperguntasabertas+1;
-		                }
-		                if(tipo.equalsIgnoreCase("Alternativa")||(tipo.equalsIgnoreCase("Alternativas"))) {
-		                    Alternativa alt = new Alternativa();
-		                    String enuciado = JOptionPane.showInputDialog("Qual o enuciado?" + (1+i));
-		                    alt.setEnuciadoPergunta(enuciado);
-		                    enunciados[i]=alt.getEnuciadoPergunta();
-		                    quantidadeitens = Integer.parseInt(JOptionPane.showInputDialog("Quantos itens terá as alternativas? "));
-		                     items = new String[quantidadeitens];
-		                    for (int m=0;m<quantidadeitens;m++){
-		                        String item = JOptionPane.showInputDialog("Qual o item " + (1+m));
-		                        items[i]=item;
-		                        
-		                    } 
-		                    quantidadedeperguntasfechadasAlt =quantidadedeperguntasfechadasAlt+1;
-		                    
-		                }
-		                    
-		                 if(tipo.equalsIgnoreCase("Lista")){
-		                     Lista list  = new Lista();
-		                     String enuciado = JOptionPane.showInputDialog("Qual o enuciado?" + (1+i));
-		                     list.setEnuciadoPergunta(enuciado);
-		                     enunciados[i]=list.getEnuciadoPergunta();
-		                     quantidadeitens = Integer.parseInt(JOptionPane.showInputDialog("Quantos itens terá a Lista? "));
-		                       items= new String[quantidadeitens];
-		                       for (int m=0;m<quantidadeitens;m++){
-		                        String item = JOptionPane.showInputDialog("Qual o item " + (1+m));
-		                        items[i]=item;
-		                    } 
-		                       perg=list;
-		                       quantidadedeperguntasfechadasList= quantidadedeperguntasfechadasList+1;
-		                 }
-		                 
-		                 if(tipo.equalsIgnoreCase("")){
-		                 
-		                 }
-		                 
-		                 if(tipo.equalsIgnoreCase("opcional")){
-		                     String enuciado = JOptionPane.showInputDialog("Qual o enuciado?" + (1+i));
-		                     Opcional op = new Opcional();
-		                     op.setEnuciadoPergunta(enuciado);
-		                     enunciados[i]= op.getEnuciadoPergunta();
-		                     perg = op;
-		                    
-		                     quantidadedeperguntasfechadasOp=quantidadedeperguntasfechadasOp+1;
-		                     
-		                    
-		                 
-		                 }
-		                    
-		                   
-		                 
-		                    
-		            
-		                 
-		                
-		                }
-		    
-		    
-		 
-		    for(int ab=0;ab<quantidadedeperguntasabertas;ab++){
-		        String resposta = JOptionPane.showInputDialog(enunciados[ab]);
-		        //aqui adiconar a pergunta na lista
-		        
-		    }
-		    
-		    
-		    for(int op=0;op<quantidadedeperguntasfechadasOp;op++ ) {
-		    	int resposta= JOptionPane.showConfirmDialog(null,enunciados[op]);
-		    	if(resposta==0) {
-		    		System.out.println("sim");
-		    	}
-		    	if(resposta==1) {
-		    		System.out.println("não");
-		    	}
-		    	
-		    	 
-		    }
-		    
-		   
-		    
-		    
+		
+		String opcoes[] = new String[quantidadeperg];
+		String enuciadoTextoCurto[]= new String[quantidadeperg];
+		String enuciadoTextoLongo[] = new String[quantidadeperg];
+		String enuciadoAlternativa[] = new String[quantidadeperg];
+		
+		
+		int quantidadepergTxtCurto=0;
+		int quantidadedepegTxtLongo=0;
+		
+		
+		
+		String itensAlternativa[]= {};
+		
+		for(int i=0;i<quantidadeperg;i++) {
+			String opcao =JOptionPane.showInputDialog("qual tipo de pergunta deseja criar?"+(1+i));
+			if(opcao.equalsIgnoreCase("texto curto") || opcao.equalsIgnoreCase("textocurto")) {
+				opcoes[i]=opcao;
+				String enuciado = JOptionPane.showInputDialog("Qual o enuciado?");
+				enuciadoTextoCurto[i]=enuciado;
+				quantidadepergTxtCurto=quantidadepergTxtCurto+1;
+				
+			}
+			if(opcao.equalsIgnoreCase("texto longo") || opcao.equalsIgnoreCase("textolongo")) {
+				opcoes[i]= opcao;
+				String enuciado = JOptionPane.showInputDialog("Qual o enuciado?");
+				enuciadoTextoLongo[i]=enuciado;
+				quantidadedepegTxtLongo=quantidadedepegTxtLongo+1;
+			}
+			
+			if(opcao.equalsIgnoreCase("Alternativa") || opcao.equalsIgnoreCase("alternativas")) { 
+				JTextField Enunciado = new JTextField();
+				JTextField quantidadedeitens = new JTextField();
+				
+				
+				Object[] fields2= {
+						"Enuciado",Enunciado,
+						"Quantidade de itens ",quantidadedeitens,
+						
+							
+		};	
+				JOptionPane.showConfirmDialog(null,fields2,"CRIAÇÃO DA ALTERNATIVA",JOptionPane.CLOSED_OPTION);
+				
+				String enunciado = Enunciado.getText().toString();
+				int quantidadei = Integer.parseInt(quantidadedeitens.getText().toString());
+				
+				enuciadoAlternativa[i]=enunciado;
+				itensAlternativa= new String[quantidadei];
+				for(int it=0;it<quantidadei;it++) {
+					String item = JOptionPane.showInputDialog("Qual o item" +(1+it));
+					itensAlternativa[it]=item;
+				}
+						
 
-			Formulario f = new Formulario(nomeFormulario,Descricao,DataInicio,DataFinal,p);
-			JOptionPane.showMessageDialog(null, 
-					"--------------Formulario criado!--------------" + "\n" +
-							"Nome do formulario: " + f.getNomeFormulario() +"\n" +
-							"Descrição do formulario: " + f.getDescricaoFormulario() + "\n" +
-							"Data de inicio: " + f.getDataInicio() + "\n " +
-							"Data de Termino: " + f.getDataFinal() + "\n " );
-
+			
+			
+			}
+			
+			
+			
+			
+			
+		}
+		
+			
+		
 		    
-			  
-		//	f.salvarFormulario(f);
+		Pergunta perg;
+		
+		String respostatipoTextoCurto[]= new String[quantidadepergTxtCurto];
+		String respostaTipoTextoLongo[] = new String[quantidadedepegTxtLongo];
+		String respostaTipoalternativa[]= {};
+		String respostaTipoLista []= {};
+		String respostatipoOpcinal[]= {};
+		for(int j=0;j<quantidadeperg;j++) {
+			if(opcoes[j].equalsIgnoreCase("Textocurto")|| opcoes[j].equalsIgnoreCase("texto curto")) {
+				perg = new TextoCurto();
+				perg.setEnuciadoPergunta(enuciadoTextoCurto[j]);
+				respostatipoTextoCurto[j]= JOptionPane.showInputDialog(enuciadoTextoCurto[j]);
+				perg.setResposta(respostatipoTextoCurto[j]);
+				System.out.println(perg.getEnuciadoPergunta());
+				System.out.println(perg.getResposta());
+			}
+			
+			if(opcoes[j].equalsIgnoreCase("textolongo")|| opcoes[j].equalsIgnoreCase("texto longo")) {
+				perg = new TextoLongo();
+				perg.setEnuciadoPergunta(enuciadoTextoLongo[j]);
+				respostaTipoTextoLongo[j]=JOptionPane.showInputDialog(enuciadoTextoLongo[j]);
+				perg.setResposta(respostaTipoTextoLongo[j]);
+				System.out.println(perg.getEnuciadoPergunta());
+				System.out.println(perg.getResposta());
+			}
+			//if(opcoes[j].equalsIgnoreCase("textolongo")|| opcoes[j].equalsIgnoreCase("texto longo")) {
+				
+			//}
+		}
+		
+		
+		
+		
+		
 		    
-		    
-		    
-		    }
+}
 				
 }            
 		                
